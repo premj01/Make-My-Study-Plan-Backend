da13df50d66b6660cedecb9694a69dcbfe93e3c4
premj01 <premjadhav00002@gmail.com>
2025-03-16 02:26:46 +0530

    v2

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { port, hostname, db } = require('./config/config')
 const router = require('./router/route')
 const authrouter = require('./router/router.auth')
 const cors = require('cors');
-const cron = require('node-cron');
 const http = require('http');
+
+const keepAlive = require('./keep-alive')
 
 const ConnectDB = require('./config/config.db');
 ConnectDB(db);
@@ -18,6 +19,9 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
+keepAlive();
+
 
 app.use("/auth", authrouter)
 app.use("/", router);
@@ -34,21 +38,7 @@ app.get("*", (req, res) => {
   res.status(200).json({ message: "Route not found" });
 });
 
-// Cron job to keep server alive - runs every 10 minutes
-cron.schedule('*/10 * * * *', () => {
-  const serverUrl = `http://${hostname}:${port}`;
-  console.log(`Ping server to keep alive at: ${new Date().toISOString()}`);
 
-  http.get(serverUrl, (res) => {
-    if (res.statusCode === 200) {
-      console.log('Server is alive');
-    } else {
-      console.log('Server ping failed with status:', res.statusCode);
-    }
-  }).on('error', (err) => {
-    console.error('Error pinging server:', err.message);
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server Started on PORT http://${hostname}:${port}`);
